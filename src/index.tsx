881ddcac136b6d24f26c58528e30d016217d85cf
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2023-02-18 16:51:21 +0100

    Add favicon and update line endings from crlf to lf

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./components/App/App";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./styles/mainTheme";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -10,7 +12,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={mainTheme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
